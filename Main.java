d160764e46ae8cf2bdc75e37d24745653f3001c7
bhawtin6 <bhawtin6@hotmail.com>
2021-04-29 14:43:22 -0400

    created basic constructor for Piece.java

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
--- Piece.java
+++ Piece.java
@@ -1,16 +1,18 @@
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import java.util.List;
 import javafx.geometry.Rectangle2D;
 
-public abstract class Piece {
+public class Piece {
     char colour; //'b' or 'w'
     int turnLastMoved; //last turn that this piece has moved
 
-    ImageView icon = new ImageView(new Image("chesspieces.png"));
-    icon.setPreserveRatio(true);
-    icon.setFitHeight(100);
-    icon.setFitWidth(100);
-    icon.setViewport(new Rectangle2D(0,0,132,132));
+    public Piece(){
+        ImageView icon = new ImageView(new Image("chesspieces.png"));
+        icon.setPreserveRatio(true);
+        icon.setFitHeight(100);
+        icon.setFitWidth(100);
+        icon.setViewport(new Rectangle2D(0,0,132,132));
+    }
+
 
 }
