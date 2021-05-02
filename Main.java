4845d1bcbe8e3d735cf0a927354aba9545e4708b
bhawtin6 <bhawtin6@hotmail.com>
2021-05-02 14:51:27 -0400

    Added FEN parser, board is now able to show starting position

--- Main.java
+++ Main.java
@@ -9,95 +9,30 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage){
 
         Image piecesImage = new Image("chesspieces.png");
 
-        //rooks
-        ImageView wRook = new ImageView(piecesImage);
-        wRook.setPreserveRatio(true);
-        wRook.setFitHeight(100);
-        wRook.setFitWidth(100);
-        wRook.setViewport(new Rectangle2D(0,0,132,132));
-        ImageView bRook = new ImageView(piecesImage);
-        bRook.setPreserveRatio(true);
-        bRook.setFitHeight(100);
-        bRook.setFitWidth(100);
-        bRook.setViewport(new Rectangle2D(0,132,132,132));
-        //knights
-        ImageView wKnight = new ImageView(piecesImage);
-        wKnight.setPreserveRatio(true);
-        wKnight.setFitHeight(100);
-        wKnight.setFitWidth(100);
-        wKnight.setViewport(new Rectangle2D(132,0,132,132));
-        ImageView bKnight = new ImageView(piecesImage);
-        bKnight.setPreserveRatio(true);
-        bKnight.setFitHeight(100);
-        bKnight.setFitWidth(100);
-        bKnight.setViewport(new Rectangle2D(132,132,132,132));
-        //bishops
-        ImageView wBishop = new ImageView(piecesImage);
-        wBishop.setPreserveRatio(true);
-        wBishop.setFitHeight(100);
-        wBishop.setFitWidth(100);
-        wBishop.setViewport(new Rectangle2D(264,0,132,132));
-        ImageView bBishop = new ImageView(piecesImage);
-        bBishop.setPreserveRatio(true);
-        bBishop.setFitHeight(100);
-        bBishop.setFitWidth(100);
-        bBishop.setViewport(new Rectangle2D(264,132,132,132));
-        //queens
-        ImageView wQueen = new ImageView(piecesImage);
-        wQueen.setPreserveRatio(true);
-        wQueen.setFitHeight(100);
-        wQueen.setFitWidth(100);
-        wQueen.setViewport(new Rectangle2D(396,0,132,132));
-        ImageView bQueen = new ImageView(piecesImage);
-        bQueen.setPreserveRatio(true);
-        bQueen.setFitHeight(100);
-        bQueen.setFitWidth(100);
-        bQueen.setViewport(new Rectangle2D(396,132,132,132));
-        //kings
-        ImageView wKing = new ImageView(piecesImage);
-        wKing.setPreserveRatio(true);
-        wKing.setFitHeight(100);
-        wKing.setFitWidth(100);
-        wKing.setViewport(new Rectangle2D(528,0,132,132));
-        ImageView bKing = new ImageView(piecesImage);
-        bKing.setPreserveRatio(true);
-        bKing.setFitHeight(100);
-        bKing.setFitWidth(100);
-        bKing.setViewport(new Rectangle2D(528,132,132,132));
-        //pawns
-        ImageView wPawn = new ImageView(piecesImage);
-        wPawn.setPreserveRatio(true);
-        wPawn.setFitHeight(100);
-        wPawn.setFitWidth(100);
-        wPawn.setViewport(new Rectangle2D(660,0,132,132));
-        ImageView bPawn = new ImageView(piecesImage);
-        bPawn.setPreserveRatio(true);
-        bPawn.setFitHeight(100);
-        bPawn.setFitWidth(100);
-        bPawn.setViewport(new Rectangle2D(660,132,132,132));
-        //blank
-        ImageView blankSpace = new ImageView(piecesImage);
-        blankSpace.setPreserveRatio(true);
-        blankSpace.setFitHeight(100);
-        blankSpace.setFitWidth(100);
-        blankSpace.setViewport(new Rectangle2D(0,0,1,1));
+        String fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        Piece[][] pieces = parseFEN(fen);
 
 
 
-
+        //drawing the board
         TilePane piecesPane = new TilePane();
         piecesPane.setPrefColumns(8);
         piecesPane.setPrefRows(8);
         piecesPane.setPrefTileHeight(100);
         piecesPane.setPrefTileWidth(100);
 
-        piecesPane.getChildren().addAll(wRook, blankSpace, bRook, wKnight, bKnight, wBishop, bBishop, wQueen, bQueen, wKing, bKing, wPawn, bPawn );
+        for (int i = 0 ; i <8; i++){
+            for (int j = 0; j <8 ; j++){
+                piecesPane.getChildren().add(pieces[i][j].icon);
+            }
+        }
 
         TilePane board = new TilePane();
         board.setPrefColumns(8);
@@ -121,8 +56,37 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
     }
+
+    public static Piece[][] parseFEN(String fen){
+        Piece[][] pieces = new Piece[8][8];
+        int rank, file;
+        String[] split1, split2;
+        split1 = fen.split(" ", 2);
+        split2 = split1[0].split("/");
+
+        for (rank = 0; rank < 8; rank++){
+            file = 0;
+            int fenMarker = 0;
+            while (file < 8 ){ //&& fenMarker < split2[rank].length()
+                char ch = split2[rank].charAt(fenMarker);
+                if (Character.isDigit(ch)){
+                    int num = Character.getNumericValue(ch);
+                    for (int i = 0;i < num; i++){
+                        pieces[rank][file] = new Piece('x');
+                        file++;
+                    }
+                }
+                else {
+                    pieces[rank][file] = new Piece(ch);
+                    file++;
+                }
+                fenMarker ++;
+            }
+        }
+        return pieces;
+    }
+
 }
--- Piece.java
+++ Piece.java
@@ -1,17 +1,70 @@
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.geometry.Rectangle2D;
+import static java.lang.Character.isLowerCase;
 
 public class Piece {
     char colour; //'b' or 'w'
     int turnLastMoved; //last turn that this piece has moved
+    ImageView icon; //rnbqk
 
-    public Piece(){
-        ImageView icon = new ImageView(new Image("chesspieces.png"));
+    public Piece(char ch){
+        turnLastMoved = -1;
+
+        int row;
+        int col;
+        if (isLowerCase(ch)){
+            colour = 'b';
+            row = 1;
+        }
+        else {
+            colour = 'w';
+            row = 0;
+        }
+        switch (ch){
+            case 'r':
+            case 'R':
+                System.out.printf("ch = %c, making a rook\n", ch);
+                col = 0;
+                break;
+            case 'n':
+            case 'N':
+                System.out.printf("ch = %c, making a knight\n", ch);
+                col = 1;
+                break;
+            case 'b':
+            case 'B':
+                System.out.printf("ch = %c, making a bishop\n", ch);
+                col = 2;
+                break;
+            case 'q':
+            case 'Q':
+                System.out.printf("ch = %c, making a queen\n", ch);
+                col = 3;
+                break;
+            case 'k':
+            case 'K':
+                System.out.printf("ch = %c, making a king\n", ch);
+                col = 4;
+                break;
+            case 'p':
+            case 'P':
+                System.out.printf("ch = %c, making a pawn\n", ch);
+                col = 5;
+                break;
+            default:
+                System.out.printf("ch = %c, making a blank space\n", ch);
+                col = 6;//blank
+        }
+        icon = new ImageView(new Image("chesspieces.png"));
         icon.setPreserveRatio(true);
         icon.setFitHeight(100);
         icon.setFitWidth(100);
-        icon.setViewport(new Rectangle2D(0,0,132,132));
+        if ( col == 6){
+            icon.setViewport(new Rectangle2D(0,0,1,1));
+            return;
+        }
+        icon.setViewport(new Rectangle2D(col*132,row*132,132,132));
     }
 
 
