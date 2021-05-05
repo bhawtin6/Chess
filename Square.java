3538eada97782ad31b1dbab2231ee9a5ad701453
bhawtin6 <bhawtin6@hotmail.com>
2021-05-05 16:52:18 -0400

    Added a turn counter system (half-turns), changed the data storage from an array of pieces to an array of squares, squares can now be highlighted

---
+++ Square.java
@@ -0,0 +1,37 @@
+import javafx.scene.paint.Color;
+
+public class Square {
+    public int rank;
+    public int file;
+    public Piece piece;
+    public boolean highlighted;
+    Color colour;
+
+    public Square(int r, int f){
+        rank = r;
+        file = f;
+        piece = new Piece('x');
+        highlighted = false;
+        updateColour();
+    }
+    public Square(int r, int f, Piece p, boolean h){
+        rank = r;
+        file = f;
+        piece = p;
+        highlighted = h;
+        updateColour();
+    }
+    public Color updateColour(){
+        if ((rank+file)%2 == 0 ) colour = Color.ALICEBLUE;
+        else colour = Color.CADETBLUE;
+        if (highlighted){
+            colour = colour.darker();
+        }
+        return this.colour;
+    }
+    public Color toggleHighlight(){
+        highlighted = !highlighted;
+        updateColour();
+        return this.colour;
+    }
+}
